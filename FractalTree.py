8644bfb9f673f61f62c6e08cd1318d3abbbefd2d
YLili <1911521164@qq.com>
2016-06-08 21:03:55 +0800

    Update FractalTree.py

--- FractalTree.py
+++ FractalTree.py
@@ -2,16 +2,16 @@ __author__ = 'Lili'
 #coding: utf-8
 import turtle
 import Queue
-import time
-import threading
+
 ##----------  Setup a queque to store the grow node ------------------------
-NodeQue = Queue.Queue()
+dNodeQue = Queue.Queue()
+NewNodeQue = Queue.Queue()
 ##----------  Initialize the parameter ------------------------
 length = 50   #  setup the branch length
 angle = 45    #  setup the branch angle
+lineWidth = 10
 turtle.speed(1)   #  setup the grow speed,the speed is 0-10,1 is the lowest, 1-10 the speed becomes faster.But 0 is the fastest.
-turtle.color('green')  # setup the line color
-turtle.pensize(5)       # setup the line width
+
 
 ##----------  Setup the class of node ------------------------
 class Node():
@@ -26,104 +26,103 @@ class Node():
     ##----------  Setup the node behavior of type a ------------------------
     def a_branch(self):
         global length
-        global NodeQue
+        global NewNodeQue
         turtle.up()
         turtle.setpos(self.NodePosition)
         turtle.down()
         turtle.setheading(self.GrowAngle)
-        turtle.forward(length)
-        d = Node('d', turtle.pos(),turtle.heading() )
-        NodeQue.put(d)
+        d = Node('d', self.NodePosition,self.GrowAngle)
+        dNodeQue.put(d)
         turtle.left(angle)
         turtle.forward(length)
         c = Node('c',turtle.pos(),turtle.heading() )
-        NodeQue.put(c)
+        NewNodeQue.put(c)
         turtle.up()
         turtle.setpos(d.NodePosition)
         turtle.down()
         turtle.setheading(d.GrowAngle)
         turtle.forward(length)
-        b = Node('b',turtle.pos(),turtle.heading() )
-        NodeQue.put(b)
+        b = Node('b',turtle.pos(),turtle.heading())
+        NewNodeQue.put(b)
     ##----------  Setup the node behavior of type b ------------------------
     def b_branch(self):
         global length
-        global NodeQue
+        global NewNodeQue
         turtle.up()
         turtle.setpos(self.NodePosition)
         turtle.down()
         turtle.setheading(self.GrowAngle)
-        turtle.forward(length/3)
-        d = Node('d',turtle.pos(),turtle.heading())
-        NodeQue.put(d)
+        d = Node('d',self.NodePosition,self.GrowAngle)
+        dNodeQue.put(d)
         turtle.right(angle)
         turtle.forward(length)
         e = Node('e',turtle.pos(),turtle.heading())
-        NodeQue.put(e)
+        NewNodeQue.put(e)
         turtle.up()
         turtle.setpos(d.NodePosition)
         turtle.down()
         turtle.setheading(d.GrowAngle)
         turtle.forward(length)
         a = Node('a',turtle.pos(),turtle.heading())
-        NodeQue.put(a)
+        NewNodeQue.put(a)
     ##----------  Setup the node behavior of type c ------------------------
     def c_branch(self):
-        global length
-        global NodeQue
-        turtle.up()
-        turtle.setpos(self.NodePosition)
-        turtle.down()
-        turtle.setheading(self.GrowAngle)
-        turtle.forward(length/3)
-        b = Node('b',turtle.pos(),turtle.heading())
-        NodeQue.put(b)
+        global NewNodeQue
+        b = Node('b',self.NodePosition,self.GrowAngle)
+        NewNodeQue.put(b)
     ##----------  Setup the node behavior of type d ------------------------
-    def d_branch(self):
-        global length
-        global NodeQue
-        d = Node('d',self.NodePosition,self.GrowAngle)
-        NodeQue.put(d)
+    #def d_branch(self):
+
     ##----------  Setup the node behavior of type e ------------------------
     def e_branch(self):
         global length
-        global NodeQue
+        global NewNodeQue
         turtle.up()
         turtle.setpos(self.NodePosition)
         turtle.down()
         turtle.setheading(self.GrowAngle)
-        turtle.forward(length/3)
-        d = Node('d',turtle.pos(),turtle.heading())
-        NodeQue.put(d)
+        d = Node('d',self.NodePosition,self.GrowAngle)
+        dNodeQue.put(d)
         turtle.left(angle)
         turtle.forward(length)
         a = Node('a',turtle.pos(),turtle.heading())
-        NodeQue.put(a)
+        NewNodeQue.put(a)
         turtle.up()
         turtle.setpos(d.NodePosition)
         turtle.down()
         turtle.setheading(d.GrowAngle)
         turtle.forward(length)
         b = Node('b',turtle.pos(),turtle.heading())
-        NodeQue.put(b)
+        NewNodeQue.put(b)
     ##----------  Run the node behavior according to its type ------------------------
     def run(self):
-        global NodeQue
+        global NewNodeQue
         if self.NodeType == 'a':
             self.a_branch()
         elif self.NodeType == 'b':
             self.b_branch()
         elif self.NodeType == 'c':
             self.c_branch()
-        elif self.NodeType == 'd':
-            self.d_branch()
+        #elif self.NodeType == 'd':
+            #self.d_branch()
         elif self.NodeType == 'e':
             self.e_branch()
 
 ##----------  Setup the first node and start to build the tree ------------------------
-FirstNode = Node('a',(0,-250),90)
-NodeQue.put(FirstNode)
-while( not NodeQue.empty()):
-    GrowNode = NodeQue.get()
+turtle.color('brown')  # setup the line color
+turtle.pensize(lineWidth*2)       # setup the line width
+turtle.up()
+turtle.setpos(0,-250)
+turtle.down()
+turtle.setheading(90)
+turtle.forward(length*2)
+turtle.color('green')  # setup the line color
+turtle.pensize(lineWidth)
+FirstNode = Node('a',turtle.pos(),turtle.heading())
+NewNodeQue.put(FirstNode)
+Index = 0
+while( (not NewNodeQue.empty()) and (Index<100) ):
+    GrowNode = NewNodeQue.get()
     GrowNode.run()
-
+    Index += 1
+turtle.done()
